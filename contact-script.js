6be58838f12b22ef5f6c5abf2e3b673d2ac4840e
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-03-26 12:36:30 +0100

    sendButton works / Personnalized Message in case of empty input

--- contact-script.js
+++ contact-script.js
@@ -1,5 +1,24 @@
-const sendButton = document.querySelector[".button button"];
+const sendButton = document.querySelector("#sendButton");
+const nameInput = document.querySelector("#name");
+const mailInput = document.querySelector("#mail");
+const messageInput = document.querySelector("#message")
 
+/* Vérifie si le format mail est valide */
+function isValidEmail(email) {
+    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+}
+
+/* Affiche un message si la valeur est vide
+/ .trim enlève les espaces devant et derrière 
+pour éviter de ne rentrer que des espaces*/
 sendButton.addEventListener("click", () => {
-    console.log("coucou")
+    if (nameInput.value.trim() === "")
+        alert("Merci d'indiquer votre nom")
+    else if (mailInput.value.trim() == "")
+        alert("Merci de renseigner votre mail")
+    else if (!isValidEmail(mailInput.value.trim()))
+        alert("Merci d'indiquer une adresse mail valide")
+    else if (messageInput.value.trim() === "")
+        alert("Veuillez préciser votre demande")
+    else { alert("Votre message a bien été envoyé") }
 })
