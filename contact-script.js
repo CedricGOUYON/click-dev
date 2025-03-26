123e0a9edfd323677b49807d0190125b12e0d3f3
Luccioni Vincent <vincent.luccioni@gmail.com>
2025-03-26 16:51:05 +0100

    JS works ! CSS in progress

--- contact-script.js
+++ contact-script.js
@@ -11,7 +11,8 @@ function isValidEmail(email) {
 /* Affiche un message si la valeur est vide
 / .trim enlève les espaces devant et derrière 
 pour éviter de ne rentrer que des espaces*/
-sendButton.addEventListener("click", () => {
+
+/*sendButton.addEventListener("click", () => {
     if (nameInput.value.trim() === "")
         alert("Merci d'indiquer votre nom")
     else if (mailInput.value.trim() == "")
@@ -20,5 +21,27 @@ sendButton.addEventListener("click", () => {
         alert("Merci d'indiquer une adresse mail valide")
     else if (messageInput.value.trim() === "")
         alert("Veuillez préciser votre demande")
-    else { alert("Votre message a bien été envoyé") }
+    else {
+        alert("Votre message a bien été envoyé")
+    }
+
+})*/
+
+
+/* Affiche le message d'alerte dans un élément à part*/
+
+sendButton.addEventListener("click", () => {
+    if (nameInput.value.trim() === "")
+        document.getElementById("valideInput").textContent = "Merci d'indiquer votre nom";
+    else if (mailInput.value.trim() == "")
+        document.getElementById("valideInput").textContent = "Merci de renseigner une adresse mail"
+    else if (!isValidEmail(mailInput.value.trim()))
+        document.getElementById("valideInput").textContent = "Merci d'indiquer une adresse mail valide"
+    else if (messageInput.value.trim() === "")
+        document.getElementById("valideInput").textContent = "Veuillez préciser votre demande"
+    else {
+        document.getElementById("valideInput").textContent = "Votre message a bien été envoyé !"
+    }
+
 })
+
