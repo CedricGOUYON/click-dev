d061ca28200737ad0d9ae1e9e3f13bfe2d83bfed
Eula-Coly <eulacoly@gmail.com>
2025-03-28 11:38:39 +0100

    page equipe terminée

--- team.js
+++ team.js
@@ -1,24 +1,33 @@
 
 const commentairebutton = document.querySelector("#commentairebutton");
+
 commentairebutton.addEventListener ("click",()=>{
 
     const nom = prompt('Veuillez entrer votre nom')
+
+    if (nom === ""){
+     alert ("Votre nom n'est pas renseigné!");
+     return;
+    } 
+
+    const commentaire = prompt('Ecrivez votre commentaire!');
     
-    if (nom !==null){
-
-        const commentaire = prompt('Ecrivez votre commentaire!');
-        
-        
-    if (commentaire !==null)  
-        alert("Merci pour votre commentaire")
-
-    
-
-        
-   } else {alert ('bye');
-
-    };
-
+   if (commentaire === ""){
+    alert ("Le commentaire est vide");
+    return;
+     
+    } 
+alert ("votre commentaire sera traité par un administrateur")
 
 });
+ 
+ 
 
+
+
+     
+
+    
+
+        
+   
